73b8760194774cd0b48ec1ee4439251058610966
Michael Manzano <mikezano@gmail.com>
2016-01-08 22:09:19 -0700

    updateing gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,33 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var ts = require('gulp-typescript');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('sass', function() {
-    gulp.src('*.scss')
+    gulp.src('sass/*.scss')
         .pipe(sass())
         .pipe(gulp.dest(function(f) {
             return f.base;
         }))
 });
 
+gulp.task('scripts', function() {
+	var tsResult = gulp.src('ts/*.ts', {base: '.'})
+                        .pipe(ts({}));
+        
+					   //.pipe(sourcemaps.init()) // This means sourcemaps will be generated 
+					  // .pipe(ts({
+						//   sortOutput: true,
+						   // ... 
+					   //}));
+	
+	return tsResult.js
+				//.pipe(concat('output.js')) // You can use other plugins that also support gulp-sourcemaps 
+				//.pipe(sourcemaps.write()) // Now the sourcemaps are added to the .js file 
+				.pipe(gulp.dest('.'));
+});
+
 gulp.task('default', function() {
-    gulp.watch('*.scss', ['sass']);
-})
+    gulp.watch('sass/**/*.scss', ['sass']);
+    gulp.watch('ts/**/*.ts', ['scripts']);
+});
